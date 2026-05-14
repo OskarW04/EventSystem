c014a453c47d14fdf5c462ac18070b50137d8d86
OskarW04 <oskarwitek04@gmail.com>
2026-05-14 17:39:14 +0200

    fix: added migration

--- EventSystem.API/Program.cs
+++ EventSystem.API/Program.cs
@@ -73,6 +73,7 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    context.Database.Migrate();
 
     if (!context.Roles.Any())
     {
